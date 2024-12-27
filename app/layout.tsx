a87913d7cfe2263fef618af293529c72689c8eee
dulanjanabandara <dulanjanauop@gmail.com>
2024-12-28 04:01:41 +0530

    feat: structured navbar component

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,12 @@
-import type { Metadata } from 'next';
-import { Inter } from 'next/font/google';
-import './globals.css';
-const inter = Inter({ subsets: ['latin'] });
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+import Navbar from "@/components/navbar/Navbar";
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: 'HomeAway Draft',
-  description: 'Feel at home, away from home.',
+  title: "HomeAway Draft",
+  description: "Feel at home, away from home.",
 };
 
 export default function RootLayout({
@@ -14,9 +15,10 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang='en' suppressHydrationWarning>
+    <html lang="en" suppressHydrationWarning>
       <body className={inter.className}>
-        <main className='container py-10'>{children}</main>
+        <Navbar />
+        <main className="container py-10">{children}</main>
       </body>
     </html>
   );
