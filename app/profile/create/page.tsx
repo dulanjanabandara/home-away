f33d00eee4e4c173ee75f6051a015c905e8fe170
dulanjanabandara <dulanjanauop@gmail.com>
2024-12-29 01:57:49 +0530

    feat: created form input component, submit button component and form container component.

--- app/profile/create/page.tsx
+++ app/profile/create/page.tsx
@@ -14,15 +14,7 @@ function CreateProfile() {
     <section>
       <h1 className="text-2xl font-semibold mb-8 capitalize">new user</h1>
       <div className="border p-8 rounded-md max-w-lg">
-        <form action={createProfileAction}>
-          <div className="mb-2">
-            <Label htmlFor="firstName">First Name</Label>
-            <Input id="firstName" name="firstName" type="text" />
-          </div>
-          <Button type="submit" size="lg">
-            Create Profile
-          </Button>
-        </form>
+        <form action={createProfileAction}></form>
       </div>
     </section>
   );
